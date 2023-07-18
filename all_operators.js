ed6536737873f4203ef374f1cf24908c6fb2e42a
vishnushirgaonkar <139764479+vishnushirgaonkar@users.noreply.github.com>
2023-07-18 19:36:54 +0530

    Add files via upload

---
+++ all_operators.js
@@ -0,0 +1,65 @@
+// Arithmetic Operators
+let num1 = 10;
+let num2 = 5;
+
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+let modulus = num1 % num2;
+
+console.log("Addition:", addition);
+console.log("Subtraction:", subtraction);
+console.log("Multiplication:", multiplication);
+console.log("Division:", division);
+console.log("Modulus:", modulus);
+
+// Comparison Operators
+let a = 5;
+let b = 10;
+
+console.log("a > b:", a > b);
+console.log("a < b:", a < b);
+console.log("a >= b:", a >= b);
+console.log("a <= b:", a <= b);
+console.log("a === b:", a === b);
+console.log("a !== b:", a !== b);
+
+// Logical Operators
+let x = true;
+let y = false;
+
+console.log("x && y:", x && y);
+console.log("x || y:", x || y);
+console.log("!x:", !x);
+console.log("!y:", !y);
+
+// Assignment Operators
+let c = 5;
+
+c += 2;
+console.log("c += 2:", c);
+
+c -= 2;
+console.log("c -= 2:", c);
+
+c *= 2;
+console.log("c *= 2:", c);
+
+c /= 2;
+console.log("c /= 2:", c);
+
+c %= 2;
+console.log("c %= 2:", c);
+
+// Unary Operators
+let d = 5;
+
+console.log("++d:", ++d);
+console.log("--d:", --d);
+
+// Ternary Operator
+let age = 18;
+let isAdult = (age >= 18) ? "Adult" : "Not Adult";
+
+console.log("Is Adult?", isAdult);
---
+++ strings.js
@@ -0,0 +1,7 @@
+// array of strings
+const strings = ["Hello", "World", "JavaScript", "Code"];
+
+// Iterate  the array and print
+for (let i = 0; i < strings.length; i++) {
+  console.log(strings[i]);
+}
